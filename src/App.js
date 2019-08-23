90e1e20f63a04d7582231c59d293fcfdda44e9d5
Oscar <oalbornoz08@gmail.com>
2019-08-23 04:56:41 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+var QRCode = require("qrcode.react");
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <QRCode size={350} value="https://github.com/skaptox" level="H" />
       </header>
     </div>
   );
