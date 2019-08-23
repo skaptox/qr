f4172c817465db412fe64e443bab687daed55c39
Oscar <oalbornoz08@gmail.com>
2019-08-23 04:57:34 -0400

    Updates

---
+++ precache-manifest.e643c2cd987f1b35676fbf210889c5d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af6f6ce66c4a4a07521c85efed52bacf",
+    "url": "/qr/index.html"
+  },
+  {
+    "revision": "7b7558994a503836d744",
+    "url": "/qr/static/css/main.0b68093d.chunk.css"
+  },
+  {
+    "revision": "2ef01b2ad9d237b285c6",
+    "url": "/qr/static/js/2.0b175e47.chunk.js"
+  },
+  {
+    "revision": "7b7558994a503836d744",
+    "url": "/qr/static/js/main.e6e199f8.chunk.js"
+  },
+  {
+    "revision": "82dbd1cde69f27e3a63c",
+    "url": "/qr/static/js/runtime~main.1c08dd2a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/qr/static/media/logo.5d5d9eef.svg"
+  }
+]);
